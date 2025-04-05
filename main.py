f2b704d2fde31fc3cf641968d2b03bdf09f3ab75
lubnaasmi <llubnaasmi@gmail.com>
2025-04-05 10:45:22 -0400

    tested main file

--- main.py
+++ main.py
@@ -9,19 +9,21 @@ def run(filepath: str):
     INSERT DOCSTRING HERE
     """
     # open the json object
-    ...
-
+    with open (filepath) as j:
+        data = json.load(j)
+        
     # extract the reviews from the json file
-    ...
+    data['results']
+
 
     # get a list of sentiments for each line using get_sentiment
-    ...
+    final_result = get_sentiment(data['results'])
 
     # plot a visualization expressing sentiment ratio
-    ...
+    make_plot(final_result)
 
     # return sentiments
-    ...
+    return(final_result)
 
 
 if __name__ == "__main__":
